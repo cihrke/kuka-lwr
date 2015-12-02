4223316d17321dfcc6b5f1816ddec5cb17512001
Marco Esposito <marco.esposito@tum.de>
2015-12-02 15:45:51 +0100

    Publish effect and primitive only once
    Sleep after creation of publishers to avoid init issues

--- lwr_controllers/tools/send_HAPIdata.py
+++ lwr_controllers/tools/send_HAPIdata.py
@@ -4,37 +4,41 @@ import rospy
 from lwr_controllers.msg import Effect
 from lwr_controllers.msg import Primitive
 
+
 # Node example class.
-class NodeExample():
+class H3DPublisher():
     # Must have __init__(self) function for a class, similar to a C++ class constructor.
     def __init__(self):
-	prim_pub = rospy.Publisher('/lwr/HapiController/primitives', Primitive, queue_size=10)
-	eff_pub = rospy.Publisher('/lwr/HapiController/effects', Effect, queue_size=10)
-	rate = rospy.Rate(1) 
+        self.eff_pub = rospy.Publisher('/lwr/HapiController/effects', Effect, queue_size=1)
+        self.prim_pub = rospy.Publisher('/lwr/HapiController/primitives', Primitive, queue_size=1)
+        rospy.sleep(1)
 
-	eff = Effect()
-	eff.type = "Spring"
-	eff.data = [0,5,0,0.15]
-	
-	prim = Primitive()
-	prim.type = "Sphere"
-	prim.surface = "FrictionSurface"
-	prim.param_surface = [0,0,0,0]
-	prim.position = [0,5,0]
-	prim.data = [0.02]
+        self.effect = Effect()
 
-	while not rospy.is_shutdown():
-	    rospy.loginfo(prim)
-	    prim_pub.publish(prim)
-	    rospy.loginfo(eff)
-	    eff_pub.publish(eff)
-	    rate.sleep()
+        self.primitive = Primitive()
+
+    def publish_primitive(self, prim_type, surface_type, surface_param, position, data):
+        self.primitive.type = prim_type
+        self.primitive.surface = surface_type
+        self.primitive.param_surface = surface_param
+        self.primitive.position = position
+        self.primitive.data = data
+        rospy.loginfo(self.primitive)
+        self.prim_pub.publish(self.primitive)
+
+    def publish_effect(self, effect_type, data):
+        self.effect.type = "Spring"
+        self.effect.data = data
+        rospy.loginfo(self.effect)
+        self.eff_pub.publish(self.effect)
 
 if __name__ == '__main__':
     # Initialize the node and name it.
     rospy.init_node('pytalker')
     
     try:
-        ne = NodeExample()
+        ne = H3DPublisher()
+        ne.publish_primitive('Sphere', 'FrictionSurface', [0, 0, 0, 0], [0, 5, 0], [0.02])
+        ne.publish_effect('Spring', [0, 5, 0, 0.15])
     except rospy.ROSInterruptException:
         pass
